bce121f666fd47f1274deda861ada68b6a492634
NaokiKojimaJapan <Naoki.Kojima.Japan@gmail.com>
2024-04-28 11:36:13 +0900

    #3 commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get 'furima/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root to: "furima#index"
   # Defines the root path route ("/")
   # root "articles#index"
 end
