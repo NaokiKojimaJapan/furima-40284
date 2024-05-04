2db086d48dc2e98e900842050025f8304e857d12
NaokiKojimaJapan <Naoki.Kojima.Japan@gmail.com>
2024-05-04 15:56:34 +0900

    ユーザー管理機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'furima/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: "furima#index"
