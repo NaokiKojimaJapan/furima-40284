fb5c0b34b1029ed300554f27cecae07341608dff
NaokiKojimaJapan <Naoki.Kojima.Japan@gmail.com>
2024-05-06 18:49:39 +0900

    商品情報のデータベースへの保存

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,23 @@
+class ItemsController < ApplicationController
+  def index
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to '/'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :content, :category_id, :status_id, :shipping_fee_id, :prefecture_id, :shipping_time_id, :price).merge(user_id: current_user.id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'furima/index'
+  get 'items/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root to: "furima#index"
+  root to: "items#index"
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :items, only: [:index, :new, :create]
 end
