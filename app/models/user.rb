25e0f625cc7987fb0b222645a34e4fa6fd564686
NaokiKojimaJapan <Naoki.Kojima.Japan@gmail.com>
2024-05-12 13:06:52 +0900

    商品一覧表示機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,5 +23,5 @@ class User < ApplicationRecord
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 
-  has_many :items
+  has_many :items, dependent: :destroy
 end
