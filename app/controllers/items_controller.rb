85ff288f2e45599bc83507bbfc46e74ee013d37f
NaokiKojimaJapan <Naoki.Kojima.Japan@gmail.com>
2024-05-14 19:28:32 +0900

    商品情報編集機能の実装#1修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
   before_action :correct_user, only: [:edit, :update]
-  before_action :set_item, only: [:create, :show, :edit]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -12,6 +12,7 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
     if @item.save
       redirect_to '/'
     else
@@ -26,7 +27,6 @@ class ItemsController < ApplicationController
   end
 
   def update
-    item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
