3e264f58b29b9347cbf17ea17c4dff47b63f8aa9
NaokiKojimaJapan <Naoki.Kojima.Japan@gmail.com>
2024-05-28 19:50:36 +0900

    #2修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -6,7 +6,7 @@ const pay = () => {
 priceInput.addEventListener("input", () => {
   const inputValue = priceInput.value;
   addTaxDom.innerHTML = Math.floor(inputValue*0.1);
-  profitDom.innerHTML = Math.floor(inputValue - inputValue * 0.1);
+  profitDom.innerHTML = inputValue - Math.floor(inputValue*0.1);
 })
 };
 
